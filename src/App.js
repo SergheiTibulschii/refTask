e5a42316b9d8967391494cec0d50f386192c2fca
Serghei.Tibulschii <serghei.tibulschii@gmail.com>
2021-03-20 18:03:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, {createRef} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        isPopupVisible: false,
+        popupPosition: 'left'
+    }
+
+    constructor() {
+        super();
+        this.containerRef = createRef()
+
+        this.handleMouseEnter = this.handleMouseEnter.bind(this)
+        this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    }
+
+    componentDidMount() {
+
+    }
+
+    handleMouseEnter(event) {
+        const { x, y, width, height } = this.containerRef.current.getBoundingClientRect()
+        this.setState({
+            isPopupVisible: true,
+            targetX: x,
+            targetY: y,
+            targetWidth: width,
+            targetHeight: height
+        })
+    }
+
+    handleMouseLeave(event) {
+        this.setState({
+            isPopupVisible: false
+        })
+    }
+
+    render() {
+        const { isPopupVisible, targetX, targetY, targetHeight, targetWidth, popupPosition } = this.state
+
+        const popupStyle = {
+            position: 'absolute',
+            width: '100px',
+            height: '100px',
+            background: 'black',
+            transform: 'translateY(-50%)'
+        }
+        if(popupPosition === 'left') {
+            popupStyle.top = `${targetY + targetHeight/2}px`
+            popupStyle.left = `${targetX - 20}px`
+            popupStyle.transform = 'translate(-100%, -50%)'
+        } else if(popupPosition === 'right') {
+            popupStyle.top = `${targetY + targetHeight/2}px`
+            popupStyle.left = `${targetX + targetWidth + 20}px`
+        }
+
+        return (
+            <div className="App">
+                <div
+                    onMouseEnter={this.handleMouseEnter}
+                    onMouseLeave={this.handleMouseLeave}
+                    className="suqare"
+                    ref={this.containerRef}
+                >
+
+                </div>
+
+                {
+                    isPopupVisible && (
+                        <div style={popupStyle}>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
